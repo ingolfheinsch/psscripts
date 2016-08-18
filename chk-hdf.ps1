0ddf8082b2078165f42fdf1f92bd7dab26c37170
ingolf heinsch <ingolf@nsynk.de>
2016-08-18 13:50:03 +0200

    add recursive and delete flag

--- chk-hdf.ps1
+++ chk-hdf.ps1
@@ -2,16 +2,27 @@
       [parameter()][alias("r")]
       $rec,
 
+      [parameter()][alias("d")]
+      $del,
+
       [Parameter(Mandatory = $true, Position = 0)]
       [string] $path
    )
    if ($rec -eq "true")
    {
-    Get-ChildItem $path -recurse -force | Where-Object {$_.mode -match "h"}
+        Get-ChildItem $path -recurse -force | Where-Object {$_.mode -match "h"}
+        if ($del -eq "true")
+        {
+            Get-ChildItem $path -recurse -force | Where-Object {$_.mode -match "h"} | Remove-Item -force
+        }
    }
-else
+    else
    {
     Get-ChildItem $path -force | Where-Object {$_.mode -match "h"}
+    if ($del -eq "true")
+        {
+             Get-ChildItem $path -force | Where-Object {$_.mode -match "h"} | Remove-Item -force
+        }
    }
 
  
