d3681167d739cac51eda906607a1a238566ac4c5
ingolf heinsch <ingolf@nsynk.de>
2016-08-18 13:34:26 +0200

    check for hidden files

---
+++ chk-hdf.ps1
@@ -0,0 +1,19 @@
+﻿param(
+      [parameter()][alias("r")]
+      $rec,
+
+      [Parameter(Mandatory = $true, Position = 0)]
+      [string] $path
+   )
+   if ($rec -eq "true")
+   {
+    Get-ChildItem $path -recurse -force | Where-Object {$_.mode -match "h"}
+   }
+else
+   {
+    Get-ChildItem $path -force | Where-Object {$_.mode -match "h"}
+   }
+
+ 
+
+
